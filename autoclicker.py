f5585004ac792631190385554c5f5112802d2b49
DarioGato2009 <dariodealmeidasensio@gmail.com>
2023-04-27 09:50:15 +0200

    cambios en el codigo mejorándolo

--- autoclicker.py
+++ autoclicker.py
@@ -1,11 +1,12 @@
 import pyautogui
 import keyboard
 def func():
+    print("para empezar a spamear el click izquierdo, presiona f1, para parar el programa, presiona f2")
     while True:
-        if keyboard.is_pressed('f7'):
+        if keyboard.is_pressed('f1'):
             while True:
                 pyautogui.tripleClick()
-                if keyboard.is_pressed('f9'):
+                if keyboard.is_pressed('f2'):
                     break
 while True:
     func()
--- test4.py
+++ test4.py
@@ -1,8 +1,16 @@
-a = 60
-import pywhatkit
-from datetime import datetime
-import time
-while True == True:
-    seconds = time.time()
-    date = datetime.fromtimestamp(seconds)
-    pywhatkit.send_mail( email_sender= "dariodealmeidaasensio@gmail.com",password="Gatito2009",subject="Hola",email_receiver = "dariodealmeidasensio@gmail.com", message="Hola, xd")
+#make a bucle in python
+import smtplib
+from email.message import EmailMessage
+
+# Set up the message
+msg = EmailMessage()
+msg['Subject'] = 'Test Email'
+msg['From'] = 'dariodealmeidaasensio@gmail.com'
+msg['To'] = 'dariodealmeidasensio@gmail.com'
+msg.set_content('This is a test email message.')
+
+# Connect to the SMTP server and send the message
+with smtplib.SMTP('smtp.gmail.com', 535) as smtp:
+    smtp.starttls()
+    smtp.login('dariodealmeidaasensio@gmail.com', 'Gatito2009')
+    smtp.send_message(msg)
